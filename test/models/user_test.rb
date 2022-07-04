ec3691ff3c050bf27eb4a6f81cecbaae8f7d8eac
Poppy Rodgers <katierodgers95@gmail.com>
2022-07-04 12:28:44 +0100

    add validation spec for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user needs a 'name' to be valid" do
+    user = users(:one)
+    user.name = nil
+    assert_not user.valid?
+  end
+
+  test "user needs a 'display_picture' to be valid" do
+    user = users(:one)
+    user.display_picture = nil
+    assert_not user.valid?
+  end
 end
