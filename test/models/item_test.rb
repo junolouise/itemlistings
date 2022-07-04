11a34a933e1208e194403a21c1cd450876188145
Poppy Rodgers <katierodgers95@gmail.com>
2022-07-04 12:24:51 +0100

    add validation specs for item

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "item needs a 'title' to be valid" do
+    item = items(:one)
+    item.title = nil
+    assert_not item.valid?
+  end
+
+  test "item needs a 'thumbnail_url' to be valid" do
+    item = items(:one)
+    item.thumbnail_url = nil
+    assert_not item.valid?
+  end
+
+  test "item needs a 'distance' to be valid" do
+    item = items(:one)
+    item.distance = nil
+    assert_not item.valid?
+  end
 end
